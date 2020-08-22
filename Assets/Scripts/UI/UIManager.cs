ca38a13f136a221a367e2f86a6e27a6e07641138
nikinov <novellenicholas@gmail.com>
2020-08-22 17:08:33 +0200

    ...

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private HealthBar HappyBar;
+    [SerializeField] private CanvasGroup BlackPanel;
+    [SerializeField] private float BlackPanelShowTransitionTime = .5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FadeBlackPanelOut();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void FadeBlackPanelIn()
+    {
+        LeanTween.alphaCanvas(BlackPanel, 1, BlackPanelShowTransitionTime);
+    }
+    public void FadeBlackPanelOut()
+    {
+        LeanTween.alphaCanvas(BlackPanel, 0, BlackPanelShowTransitionTime);
+    }
+    public void SettHappyBar()
+    {
+
+    }
+}
